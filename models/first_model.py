59ecf96e583355b39f644c92d28363f8287d2408
maxime <maxime.richaudeau1@gmail.com>
2023-11-25 12:53:13 +0100

    Add a first model as example

---
+++ models/first_model.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+from bson import ObjectId
+
+class UserMongoDB(BaseModel):
+    _id: ObjectId  # Use PyMongo's ObjectId type
+    name: str
+    first_name: str
+    age: int
