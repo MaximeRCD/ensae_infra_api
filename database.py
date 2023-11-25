694dc6a5f58a4d0dd0e0b91888536e40f449e804
maxime <maxime.richaudeau1@gmail.com>
2023-11-25 10:27:29 +0100

    Add database connection

--- database.py
+++ database.py
@@ -1,14 +1,7 @@
 from pymongo.mongo_client import MongoClient
 
 # Replace the placeholder with your Atlas connection string
-uri = "<connection string>"
+uri = "mongodb://localhost:27017"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
-
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
