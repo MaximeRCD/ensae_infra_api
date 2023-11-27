72e570a16513f5ade3703bd1ff338ab8cc0ed044
maxime <maxime.richaudeau1@gmail.com>
2023-11-27 10:25:14 +0100

    Incorporate environement variables to ease developpement and local staging docker deployement

--- database.py
+++ database.py
@@ -1,9 +1,19 @@
 from pymongo.mongo_client import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+env = os.getenv("ENV")
+db_address = os.getenv(f"{env}_DB_ADDRESS")
+db_port = os.getenv(f"{env}_DB_PORT")
+db_name = os.getenv(f"{env}_DB_NAME")
+
 
 # Replace the placeholder with your Atlas connection string
-uri = "mongodb://localhost:27017"
+uri = f"mongodb://{db_address}:{db_port}"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
 
-MARMYTHON_DB = client.get_database("MARMYTHON")
+MARMYTHON_DB = client.get_database(f"{db_name}")
