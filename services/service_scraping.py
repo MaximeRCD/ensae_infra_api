7f2ad6e3aeabbfd9dabcc62872889e32a5dd7cb6
emerubis <paulguillermit@gmail.com>
2023-12-20 12:00:15 +0100

    parse quantity as float

--- services/service_scraping.py
+++ services/service_scraping.py
@@ -6,7 +6,8 @@ import urllib.request
 import re
 import ssl
 
-#returns soup of first result recipe from a query
+# returns soup of first result recipe from a query
+
 
 def get_url_from_query(query_dict):
     base_url = "http://www.marmiton.org/recettes/recherche.aspx?"
@@ -19,28 +20,28 @@ def get_url_from_query(query_dict):
     response = opener.open(url)
     html_content = response.read()
 
-    soup = BeautifulSoup(html_content, 'html.parser')
+    soup = BeautifulSoup(html_content, "html.parser")
     search_data = []
 
     articles = soup.findAll("a", href=True)
-    articles = [a for a in articles if a["href"].startswith("/recettes/recette_")]  
+    articles = [a for a in articles if a["href"].startswith("/recettes/recette_")]
     fichier = articles[0]
 
     iterarticles = iter(articles)
     for article in iterarticles:
         data = {}
         try:
-            data["name"] = article.find("h4").get_text().strip(' \t\n\r')
-            data["url"] = article['href']
+            data["name"] = article.find("h4").get_text().strip(" \t\n\r")
+            data["url"] = article["href"]
             try:
                 data["rate"] = article.find("span").get_text().split("/")[0]
             except Exception as e0:
                 pass
             try:
-                data["image"] = article.find('img')['data-src']
+                data["image"] = article.find("img")["data-src"]
             except Exception as e1:
                 try:
-                    data["image"] = article.find('img')['src']
+                    data["image"] = article.find("img")["src"]
                 except Exception as e1:
                     pass
                 pass
@@ -48,21 +49,20 @@ def get_url_from_query(query_dict):
             pass
         if data:
             search_data.append(data)
-        
-    uri = search_data[0]['url']
+
+    uri = search_data[0]["url"]
     base_url = "http://www.marmiton.org"
     url = base_url + ("" if uri.startswith("/") else "/") + uri
     return url
 
-def get_url_from_string(string):
 
-    query_dict = {'aqt': string}
+def get_url_from_string(string):
+    query_dict = {"aqt": string}
 
     return get_url_from_query(query_dict)
 
+
 def get_soup_from_url(url):
-
-
     try:
         handler = urllib.request.HTTPSHandler(context=ssl._create_unverified_context())
         opener = urllib.request.build_opener(handler)
@@ -71,41 +71,60 @@ def get_soup_from_url(url):
     except urllib.error.HTTPError as e:
         raise RecipeNotFound if e.code == 404 else e
 
-    soup = BeautifulSoup(html_content, 'html.parser')
+    soup = BeautifulSoup(html_content, "html.parser")
 
     return soup
 
+
 def _get_name(soup):
-	return soup.find("h1").get_text().strip(' \t\n\r')
+    return soup.find("h1").get_text().strip(" \t\n\r")
 
 
 def _get_ingredients(soup):
-      ingredients = [i.get_text().strip(' \t\n\r') for i in soup.findAll("span", {"class": "ingredient-name"})]
-      return ingredients
+    ingredients = [
+        i.get_text().strip(" \t\n\r")
+        for i in soup.findAll("span", {"class": "ingredient-name"})
+    ]
+    return ingredients
+
 
 def _get_quantities(soup):
-    count = [i.get_text().strip(' \t\n\r') for i in soup.findAll("span", {"class": "count"})]
+    count = [
+        i.get_text().strip(" \t\n\r") for i in soup.findAll("span", {"class": "count"})
+    ]
+    for i, q in enumerate(count):
+        if q == "":
+            count[i] = 0.0
+        else:
+            count[i] = float(q)
     return count
 
 
 def _get_units(soup):
-    units = [i.get_text().strip(' \t\n\r') for i in soup.findAll("span", {"class": "unit"})]
+    units = [
+        i.get_text().strip(" \t\n\r") for i in soup.findAll("span", {"class": "unit"})
+    ]
     return units
+
 
 def _get_nb_persons(soup):
     input_element = soup.find("div", {"class": "mrtn-recette_ingredients-counter"})
-    nb = input_element.get('data-servingsnb')
-    #print('(pour ' + nb + ' personnes)')
-    return nb    
+    nb = input_element.get("data-servingsnb")
+    # print('(pour ' + nb + ' personnes)')
+    return nb
 
-def aggregate(ingredient,unit,count):
+
+def aggregate(ingredient, unit, count):
     ingredients = []
     for i in range(len(ingredient)):
-        ingredients.append({'name':ingredient[i],'quantity':count[i],'unit':unit[i]})
+        ingredients.append(
+            {"name": ingredient[i], "quantity": count[i], "unit": unit[i]}
+        )
     return ingredients
 
+
 ### importer model recipe
-def get_recipe_from_scrap(string):  ### refaire le return 
+def get_recipe_from_scrap(string):  ### refaire le return
     url = get_url_from_string(string)
     soup = get_soup_from_url(url)
     name = _get_name(soup)
@@ -113,8 +132,14 @@ def get_recipe_from_scrap(string):  ### refaire le return
     count = _get_quantities(soup)
     unit = _get_units(soup)
     nb_persons = _get_nb_persons(soup)
-    ingredients = aggregate(ingredients,unit,count)
-    recipe={"name": name, "url": url, "ingredients": ingredients, "nb_persons": nb_persons}
+    ingredients = aggregate(ingredients, unit, count)
+    recipe = {
+        "name": name,
+        "url": url,
+        "ingredients": ingredients,
+        "nb_persons": nb_persons,
+    }
     return recipe
 
-#print(_get_recipe_from_scrap("poulet")) pour tester
+
+# print(_get_recipe_from_scrap("poulet")) pour tester
